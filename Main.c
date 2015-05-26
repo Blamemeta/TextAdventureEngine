9b0aa1328a78215b372bb64fce0668ef7f6d1bb4
Blamemeta <blamemeta@gmail.com>
2015-05-26 18:17:52 -0500

    001

--- Main.c
+++ Main.c
@@ -1,6 +1,26 @@
 #include<stdio.h>
 #include<windows.h>
 
+struct menuItem
+{
+	int (*code)(); //ptr to a int function. 
+	char * item;
+};
+
+struct menu
+{
+	struct menuItem * items[10];
+};
+
+struct dataStruct
+{
+	struct menu * cur_menu;
+	struct menu * menuList;
+	int numMenus;
+};
+
+struct dataStruct data;
+
 int init();
 int logic();
 int render();
@@ -8,6 +28,12 @@ int input();
 
 int main()
 {
-	
+	init();
+	while(true)
+	{
+		render();
+		input();
+		logic();
+	}
 	return 0;
 }
